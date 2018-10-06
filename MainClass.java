204a4f91e25d856879ef78e268ee1c046c223e72
Saurabh Mhatre <mhatresaurabh@ubuntu.ubuntu-domain>
2018-10-06 20:01:53 +0530

    set some values

--- MainClass.java
+++ MainClass.java
@@ -2,6 +2,8 @@ public class MainClass{
 	public static void main(String[] args){
 		Person person = new Person();
 		person.setName("Rahul");
+		person.setCity("Mumbai");
+		person.setAge(20);
 		System.out.println(person.getName);
 	}
 }
