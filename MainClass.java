c2049a8c4e8fdb9011b0c9bf66f7b4ce52fce97d
Saurabh Mhatre <mhatresaurabh@ubuntu.ubuntu-domain>
2018-10-06 19:42:36 +0530

    first commit to github

---
+++ MainClass.java
@@ -0,0 +1,7 @@
+public class MainClass{
+	public static void main(String[] args){
+		Person person = new Person();
+		person.setName("Rahul");
+		System.out.println(person.getName);
+	}
+}
---
+++ Person.java
@@ -0,0 +1,24 @@
+public class Person{
+	private String name;
+	private String city;
+	private Integer age;
+
+	public String getName(){
+		return this.name;
+	}
+	public String getCity(){
+		return this.city;
+	}
+	public Integer getAge(){
+		return this.age;
+	}	
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setCity(String city){
+		this.city = city;
+	}
+	public void setAge(Integer age){
+		this.age = age;
+	}
+}
