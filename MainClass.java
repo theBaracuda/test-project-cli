8d4a1ff3bb25eca1c3f7877a586f644e2ba90e29
theBaracuda <saurabh.mhatre04@yahoo.in>
2018-10-18 20:33:01 +0530

    Added properties file

--- MainClass.java
+++ MainClass.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 public class MainClass{
 	public static void main(String[] args){
 		Person person = new Person();
@@ -7,5 +10,13 @@ public class MainClass{
 		System.out.println(person.getName());
 		System.out.println(person.getCity());
 		System.out.println(person.getAge());
+		try{
+			Properties prop = new Properties();		
+			FileInputStream fis = new FileInputStream("application.properties");
+			prop.load(fis);
+			System.out.println(prop.getProperty("key"));
+		} catch(Excepion ex){
+			ex.printStackTrace();
+		}
 	}
 }
